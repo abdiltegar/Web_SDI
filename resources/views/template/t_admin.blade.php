30f74e398e562da4c11ee478529e7952c544df33
abdiltegar <abdiltegar12@gmail.com>
2017-09-08 08:56:00 +0700

    commit

--- resources/views/template/t_admin.blade.php
+++ resources/views/template/t_admin.blade.php
@@ -18,28 +18,28 @@
         <script src="{{ URL::asset('assets/js/jquery-3.2.1.min.js') }}"></script>
         <script>
         <?php date_default_timezone_set('Asia/Jakarta'); ?>
-  //buat object date berdasarkan waktu di server
-  var serverTime = new Date(<?php print date('Y, m, d, H, i, s, 0'); ?>);
-  //buat object date berdasarkan waktu di client
-  var clientTime = new Date();
-  //hitung selisih
-  var Diff = serverTime.getTime() - clientTime.getTime();
-  //fungsi displayTime yang dipanggil di bodyOnLoad dieksekusi tiap 1000ms = 1detik
-  function displayServerTime(){
-      //buat object date berdasarkan waktu di client
-      var clientTime = new Date();
-      //buat object date dengan menghitung selisih waktu client dan server
-      var time = new Date(clientTime.getTime() + Diff);
-      //ambil nilai jam
-      var sh = time.getHours().toString();
-      //ambil nilai menit
-      var sm = time.getMinutes().toString();
-      //ambil nilai detik
-      var ss = time.getSeconds().toString();
-      //tampilkan jam:menit:detik dengan menambahkan angka 0 jika angkanya cuma satu digit (0-9)
-      document.getElementById("clock").innerHTML = (sh.length==1?"0"+sh:sh) + ":" + (sm.length==1?"0"+sm:sm) + ":" + (ss.length==1?"0"+ss:ss);
-  }
-</script>
+        //buat object date berdasarkan waktu di server
+        var serverTime = new Date(<?php print date('Y, m, d, H, i, s, 0'); ?>);
+        //buat object date berdasarkan waktu di client
+        var clientTime = new Date();
+        //hitung selisih
+        var Diff = serverTime.getTime() - clientTime.getTime();
+        //fungsi displayTime yang dipanggil di bodyOnLoad dieksekusi tiap 1000ms = 1detik
+        function displayServerTime(){
+          //buat object date berdasarkan waktu di client
+          var clientTime = new Date();
+          //buat object date dengan menghitung selisih waktu client dan server
+          var time = new Date(clientTime.getTime() + Diff);
+          //ambil nilai jam
+          var sh = time.getHours().toString();
+          //ambil nilai menit
+          var sm = time.getMinutes().toString();
+          //ambil nilai detik
+          var ss = time.getSeconds().toString();
+          //tampilkan jam:menit:detik dengan menambahkan angka 0 jika angkanya cuma satu digit (0-9)
+          document.getElementById("clock").innerHTML = (sh.length==1?"0"+sh:sh) + ":" + (sm.length==1?"0"+sm:sm) + ":" + (ss.length==1?"0"+ss:ss);
+        }
+        </script>
     </head>
     <body onload="setInterval('displayServerTime()', 1000);">
 
@@ -50,18 +50,7 @@
             <ul class="menu menu-hijau header-hijau" style="margin-bottom:0px;">
               <a href="#" style="float:left;"><li><h5>Admin Page Menu</h5></li></a>
               <a href="#" style="float:right;" id="profil-menu"><li ><span><i class="fa fa-user"></i></span> Profil
-                <ul id="profil-menu-isi" class="dropdown-menu" style="color:green;float:left;">
-                  coba
-                  <li style="color:green;">coba coba</li>
-                </ul>
               </li></a>
-              <script>
-              $(document).ready(function(){
-                $("#profil-menu").click(function(){
-                  $("#profil-menu-isi").css({"display": "block"});
-                });
-              });
-              </script>
               <a href="{{ url('/admin-berita') }}" style="float:right;"><li ><span><i class="fa fa-globe"></i></span> Baru</li></a>
               <a href="{{ url('/') }}" style="float:right;"><li ><span><i class="fa fa-globe"></i></span> Lihat Web</li></a>
             </ul>
